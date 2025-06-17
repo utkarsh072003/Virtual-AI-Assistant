82c4eba59c8e93a31fdbf9f6d79c891b5cd59ab5
UTKARSH KUMAR <128170769+utkarsh072003@users.noreply.github.com>
2025-06-17 14:24:44 +0530

    Update multer.js
    change in multer

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -1,14 +1,6 @@
 import multer from "multer";
 
+const storage = multer.memoryStorage(); // ✅ store in memory
+const upload = multer({ storage });
 
-const storage = multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,"./public")
-    },
-    filename:(req, file,cb)=>{
-        cb(null, file.originalname)
-    }
-})
-
-const upload = multer({storage})
-export default upload
+export default upload;
